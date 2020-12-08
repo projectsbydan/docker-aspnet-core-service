d1dda24aba671d62f12473213aa5884b9cebe23d
Dan Deaconu <36627723+projectsbydan@users.noreply.github.com>
2020-12-08 14:39:34 +0100

    added basic web api service

---
+++ Controllers/ScalingController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace docker_aspnet_core_service.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ScalingController : ControllerBase
+    {
+
+        private readonly ILogger<ScalingController> _logger;
+
+        public ScalingController(ILogger<ScalingController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "i am alive";
+        }
+    }
+}
